185e27de8d83f45d4501c076f7d72930bef0b903
Michael Fuerst <mail@michaelfuerst.de>
2017-12-05 01:45:43 +0100

    Added alexnet, googlenet, vgg16 baseline models. Added a dataset folder structure parser.

---
+++ datasets/classification/named_folders.py
@@ -0,0 +1,42 @@
+import os
+from scipy.misc import imread
+from random import shuffle
+
+
+def data_generator(base_dir, phase):
+    classes_dir = os.path.join(base_dir, phase)
+    classes = os.listdir(classes_dir)
+    images = []
+    imgs_per_class = {}
+    for c in classes:
+        imgs_per_class[c] = 0
+        class_dir = os.path.join(classes_dir, c)
+        for filename in os.listdir(class_dir):
+            if filename.endswith(".png"):
+                images.append((os.path.join(class_dir, filename), c))
+                imgs_per_class[c] += 1
+
+    def gen():
+        while True:
+            # Shuffle data
+            shuffle(images)
+            for date in images:
+                yield (imread(date[0], mode="RGB"), date[1])
+
+    return imgs_per_class, gen()
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    imgs_per_class, gen = data_generator("D:\Data\Projects\CVHCI-Lecture-Assignments\A2\data", "test")
+
+    print("Classes and image count:")
+    print(imgs_per_class)
+
+    img, class_name = next(gen)
+    print("Image shape:")
+    print(img.shape)
+
+    for img, class_name in gen:
+        plt.imshow(img)
+        plt.show()
