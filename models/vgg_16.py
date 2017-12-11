92595a9fcb300ca6e305db27be07ca2a625b9ebb
Michael Fürst <mail@michaelfuerst.de>
2017-12-11 15:52:00 +0100

    Added flag to get only encoder part of vgg16.

--- models/vgg_16.py
+++ models/vgg_16.py
@@ -21,7 +21,7 @@ def prepare_data(img):
     return img
 
 
-def vgg16(weights_path=None):
+def vgg16(weights_path=None, remove_classifier=False):
     model = Sequential()
     model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
@@ -59,12 +59,13 @@ def vgg16(weights_path=None):
     model.add(Convolution2D(512, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1000, activation='softmax'))
+    if not remove_classifier:
+        model.add(Flatten())
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(4096, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(1000, activation='softmax'))
 
     if weights_path:
         model.load_weights(weights_path)
